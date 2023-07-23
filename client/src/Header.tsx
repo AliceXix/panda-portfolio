cc2854a0820c286c59a6c2a20c04de06e488eb28
Alice Jost <alice.jost@outlook.de>
2023-07-23 21:12:54 +0200

    set basic structure header

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function Header() {
-    
+
     return (
         <>
+        <header>
+
+        </header>
+        <h1>
+            Bean
+        </h1>
+        <main>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eum delectus odio voluptates harum magnam, assumenda officia culpa vero reprehenderit expedita reiciendis, deleniti doloribus, voluptatem impedit modi blanditiis sint asperiores!
+        </main>
         </>
     );
 };
