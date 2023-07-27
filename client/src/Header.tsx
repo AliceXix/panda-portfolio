5bb3eef807befe802eb6d69a1a58b3acc6b7138a
Alice Jost <alice.jost@outlook.de>
2023-07-27 11:03:11 +0200

    add links in header

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -4,16 +4,27 @@ import "./App.css";
 export default function Header() {
 
     return (
-        <>
-        <header>
-
-        </header>
-        <h1>
-            Bean
-        </h1>
-        <main>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eum delectus odio voluptates harum magnam, assumenda officia culpa vero reprehenderit expedita reiciendis, deleniti doloribus, voluptatem impedit modi blanditiis sint asperiores!
-        </main>
-        </>
+      <>
+        <div className="header">
+          <header>
+            <div>
+              <a href="http://google.com">Contact</a>
+              <a href="http://google.com">Portfolio</a>
+              <a href="http://google.com">Logo</a>
+            </div>
+          </header>
+          <div className="centred-div">
+            <div>
+              <h1>Bean</h1>
+              <main>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi eum delectus odio voluptates harum magnam, assumenda
+                officia culpa vero reprehenderit expedita reiciendis, deleniti
+                doloribus, voluptatem impedit modi blanditiis sint asperiores!
+              </main>
+            </div>
+          </div>
+        </div>
+      </>
     );
 };
