901b65c238ce679cf2fc3a4693a7591f50ad4411
Alice Jost <alice.jost@outlook.de>
2023-07-31 20:45:11 +0200

    import new css file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
 import Header from './views/Header';
 import Values from './views/Values';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header/>
       <Values/>
     </div>
   );
