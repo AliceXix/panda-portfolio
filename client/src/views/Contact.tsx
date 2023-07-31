a0e847583b9a2a263c9c39edaf54f4c4c16b12ae
Alice Jost <alice.jost@outlook.de>
2023-08-01 00:18:01 +0200

    rough layout contact

--- client/src/views/Contact.tsx
+++ client/src/views/Contact.tsx
@@ -4,6 +4,31 @@ export default function Contact() {
 
     return (
       <>
+        <div className="contact">
+          <form className="centred-div">
+            <div>
+              <input type="text" id="name" name="name" value="Your Name" />
+              <input type="text" id="email" name="email" value="Your Email" />
+              <input type="submit" value="Submit" />
+            </div>
+            <div>
+              <textarea
+                id="questions"
+                name="questions"
+                value={"Share your thoughts"}
+                rows={20}
+                cols={60}
+              ></textarea>
+            </div>
+          </form>
+          <footer>
+            <div>
+              <h4>
+                <u>Artists we support</u>
+              </h4>
+            </div>
+          </footer>
+        </div>
       </>
     );
 }
