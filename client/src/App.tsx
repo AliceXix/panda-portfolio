4f41c8b6f538ad7b3b9b4bd4af0b07dffb9a8d4f
Alice Jost <alice.jost@outlook.de>
2023-07-31 23:27:35 +0200

    set up contact view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './index.css';
 import Header from './views/Header';
 import Values from './views/Values';
+import Contact from './views/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Values/>
+      <Contact/>
     </div>
   );
 }
