441449d6a99648a75801b29971435eb8ec12a13c
Alice Jost <alice.jost@outlook.de>
2023-08-01 17:44:56 +0200

    basic set up for portfolio view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import Header from './views/Header';
 import Values from './views/Values';
 import Contact from './views/Contact';
+import Portfolio from './views/Portolio';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Values/>
+      <Portfolio/>
       <Contact/>
     </div>
   );
---
+++ client/src/views/Portolio.tsx
@@ -0,0 +1,8 @@
+export default function Portfolio() {
+
+    return (
+        <>
+        <h4>Contact</h4>
+        </>
+    );
+};
