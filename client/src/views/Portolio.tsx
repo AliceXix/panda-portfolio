a9f24b05def9b378bd9d732603aa01966dc789e1
Alice Jost <alice.jost@outlook.de>
2023-08-01 18:02:41 +0200

    basic layout portfoolio view

--- client/src/views/Portolio.tsx
+++ client/src/views/Portolio.tsx
@@ -1,8 +1,20 @@
 export default function Portfolio() {
 
     return (
-        <>
-        <h4>Contact</h4>
-        </>
+      <>
+        <div className="portfolio">
+          <section>Leave blank for portfolio grid</section>
+          <section className="centred-div">
+            <div>
+              <h2>CONTACT</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                sagittis ipsum nec sapien semper gravida. Aliquam et enim
+                libero.
+              </p>
+            </div>
+          </section>
+        </div>
+      </>
     );
 };
