5187f6aaa2e942d24ee2bbbb2e4997d8525fadbb
Alice Jost <alice.jost@outlook.de>
2024-11-08 12:41:43 +0100

    fix orientation hook to trigger on mobile

--- client/src/hooks/useOrientationLock.tsx
+++ client/src/hooks/useOrientationLock.tsx
@@ -3,40 +3,66 @@ import { useEffect, useState } from "react";
 export default function useOrientationLock(): boolean {
   const [isLandscape, setIsLandscape] = useState(false);
 
-  const handleOrientationChange = () => {
-    const isMobile = window.innerWidth < 767;
-    if (isMobile && window.screen.orientation) {
-      const isLandscapeNow =
-        window.screen.orientation.type.startsWith("landscape");
-      setIsLandscape(isLandscapeNow);
+  const checkOrientation = () => {
+    const isMobile =
+      /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) ||
+      window.innerWidth < 767;
+    const isLandscapeNow = window.innerHeight < window.innerWidth;
+
+    if (isMobile && isLandscapeNow) {
+      setIsLandscape(true);
     } else {
       setIsLandscape(false);
     }
   };
 
   useEffect(() => {
-    handleOrientationChange(); // Check initial orientation
+    checkOrientation();
 
-    // Add event listener for orientation change using screen.orientation
-    if (window.screen.orientation) {
-      window.screen.orientation.addEventListener(
-        "change",
-        handleOrientationChange
-      );
-    }
-    window.addEventListener("resize", handleOrientationChange);
+    window.addEventListener("resize", checkOrientation);
+    window.addEventListener("orientationchange", checkOrientation);
 
-    // Cleanup listener on component unmount
     return () => {
-      if (window.screen.orientation) {
-        window.screen.orientation.removeEventListener(
-          "change",
-          handleOrientationChange
-        );
-      }
-      window.removeEventListener("resize", handleOrientationChange);
+      window.removeEventListener("resize", checkOrientation);
+      window.removeEventListener("orientationchange", checkOrientation);
     };
   }, []);
+
+  //   const handleOrientationChange = () => {
+  //     let isMobile = window.innerWidth < 767;
+  //     isMobile = true;
+  //     if (isMobile && window.screen.orientation) {
+  //       const isLandscapeNow =
+  //         window.screen.orientation.type.startsWith("landscape");
+  //       setIsLandscape(isLandscapeNow);
+  //     } else {
+  //       setIsLandscape(false);
+  //     }
+  //   };
+
+  //   useEffect(() => {
+  //     handleOrientationChange(); // Check initial orientation
+
+  //     // Add event listener for orientation change using screen.orientation
+  //     if (window.screen.orientation) {
+  //       window.screen.orientation.addEventListener(
+  //         "change",
+  //         handleOrientationChange
+  //       );
+  //     }
+  //     window.addEventListener("resize", handleOrientationChange);
+
+  //     // Cleanup listener on component unmount
+  //     return () => {
+  //       if (window.screen.orientation) {
+  //         window.screen.orientation.removeEventListener(
+  //           "change",
+  //           handleOrientationChange
+  //         );
+  //       }
+  //       window.removeEventListener("resize", handleOrientationChange);
+  //     };
+  //   }, []);
 
   return isLandscape;
 }
