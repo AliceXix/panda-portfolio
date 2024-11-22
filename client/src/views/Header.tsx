290181b435b2e15f9a7434c3e16e7245a08d3081
Alice Jost <alice.jost@outlook.de>
2024-11-22 20:35:30 +0100

    inverted menu order for optics

--- client/src/views/Header.tsx
+++ client/src/views/Header.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../images/insta-logo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/insta-logo.png";
 
 export default function Header() {
-
-    return (
-      <>
-        <div className="header">
-          <header>
-            <a className='nav_menu' href="#contact">Contact</a>
-            <a className='nav_menu' href="#portfolio">Portfolio</a>
-            <a href="https://www.instagram.com/solanum_bean/" target='blank'>
-              <img src={logo} alt="insta logo" />
-            </a>
-          </header>
-          <section className="centred-div">
-            <div>
-              <h1>Bean</h1>
-              <main>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi eum delectus odio voluptates harum magnam, assumenda
-                officia culpa vero reprehenderit expedita reiciendis, deleniti
-                doloribus, voluptatem impedit modi blanditiis sint asperiores!
-              </main>
-            </div>
-          </section>
-        </div>
-      </>
-    );
-};
+  return (
+    <>
+      <div className="header">
+        <header>
+          <a href="https://www.instagram.com/solanum_bean/" target="blank">
+            <img src={logo} alt="insta logo" />
+          </a>
+          <a className="nav_menu" href="#contact">
+            Contact
+          </a>
+          <a className="nav_menu" href="#portfolio">
+            Portfolio
+          </a>
+        </header>
+        <section className="centred-div">
+          <div>
+            <h1>Bean</h1>
+            <main>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+              eum delectus odio voluptates harum magnam, assumenda officia culpa
+              vero reprehenderit expedita reiciendis, deleniti doloribus,
+              voluptatem impedit modi blanditiis sint asperiores!
+            </main>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
