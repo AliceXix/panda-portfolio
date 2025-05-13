ac29fc2282d611e329817acca9d35b50a408db8a
Alice Jost <alice.jost@outlook.de>
2025-05-13 16:40:23 +0200

    adapt PORT for production

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 dotenv.config();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 console.log("GMAIL_USER:", process.env.GMAIL_USER);
 console.log("GMAIL_PASS:", process.env.GMAIL_PASS);
