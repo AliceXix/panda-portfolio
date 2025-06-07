c1d3ba8233d978bc2903bc6062ee0448e4b11cd0
Alice Jost <alice.jost@outlook.de>
2025-06-07 14:45:40 +0200

    change caroussel to fix background image

--- client/src/utils/Carousel-header.tsx
+++ client/src/utils/Carousel-header.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Carousel } from "antd";
 
-import img1 from "../images/portfolio/IMG_7869.jpeg";
-import img2 from "../images/portfolio/IMG_8437.jpeg";
-import img3 from "../images/portfolio/IMG_8513.jpeg";
-import img4 from "../images/portfolio/IMG_7317.jpeg";
+import imgI from "../images/portfolio/logo-panda.jpg";
 
 function FullScreenImage({ backgroundImgUrl }: { backgroundImgUrl: string }) {
   const contentStyle: React.CSSProperties = {
@@ -25,16 +22,7 @@ export default function CarouselHeader() {
   return (
     <Carousel autoplay className="carousel">
       <div>
-        <FullScreenImage backgroundImgUrl={img1} />
-      </div>
-      <div>
-        <FullScreenImage backgroundImgUrl={img2} />
-      </div>
-      <div>
-        <FullScreenImage backgroundImgUrl={img3} />
-      </div>
-      <div>
-        <FullScreenImage backgroundImgUrl={img4} />
+        <FullScreenImage backgroundImgUrl={imgI} />
       </div>
     </Carousel>
   );
